8476d4c8e5fa67525290539c1425aaf93b010e38
Monika <monika.frelek@gmail.com>
2022-01-06 15:55:22 +0100

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class)->withTimestamps();;
+    }
+
+    public function assignRole($role)
+    {
+        if(is_string($role)) {
+            $role = Role::whereName($role)->firstOrFail();
+        }
+        $this->roles()->sync($role, false);
+    }
+
+    public function hasRole($role)
+    {
+        if ($this->roles()->where('name', $role)->first()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function hasAnyRole($roles)
+    {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function isAdministrator()
+    {
+        if($this->hasAnyRole('admin')){
+            return true;
+        }
+    }
+
+    public function authorizeRoles($roles)
+    {
+        if ($this->hasAnyRole($roles)) {
+            return true;
+        }
+        abort(401, 'This action is unauthorized.');
+    }
+}
+
+// $user->role()->save($role);
+// $user->assignRole();
+// $user->role->map->abilities;
+// in model func some() return $this-> $user->roles->map->abilities->flatten()->plack('name')->unique;
+// in blade @can('admin') @endcan hook in gate AuthServiceProvider boot
+// authprovider - auth()->user()->abilities()->each() instead
